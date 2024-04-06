e79b14802a9f297efdd611b626f0d8400727f5bf
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2024-04-06 12:52:32 +0700

    merge and restructure

--- Ntier.DAL/Context/ShopContext.cs
+++ Ntier.DAL/Context/ShopContext.cs
@@ -17,7 +17,10 @@ namespace Ntier.DAL.Context
         {
         }
 
+        public virtual DbSet<Cart> Carts { get; set; } = null!;
+        public virtual DbSet<CartDetail> CartDetails { get; set; } = null!;
         public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Comment> Comments { get; set; } = null!;
         public virtual DbSet<Order> Orders { get; set; } = null!;
         public virtual DbSet<OrderDetail> OrderDetails { get; set; } = null!;
         public virtual DbSet<Product> Products { get; set; } = null!;
@@ -25,10 +28,90 @@ namespace Ntier.DAL.Context
         public virtual DbSet<ProductSize> ProductSizes { get; set; } = null!;
         public virtual DbSet<ProductSizeDetail> ProductSizeDetails { get; set; } = null!;
         public virtual DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
+        public virtual DbSet<StatusOrder> StatusOrders { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-O1721LD\\HBI;Database=MIKI_SHOP;UID=sa;PWD=d11052003;");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Cart>(entity =>
+            {
+                entity.ToTable("CART");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.CreateAt)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("CREATE_AT");
+
+                entity.Property(e => e.UserId)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("USER_ID");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Carts)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__CART__USER_ID__690797E6");
+            });
+
+            modelBuilder.Entity<CartDetail>(entity =>
+            {
+                entity.HasKey(e => new { e.CartId, e.ProductId })
+                    .HasName("PK__CART_DET__8E2AFE19640620D4");
+
+                entity.ToTable("CART_DETAIL");
+
+                entity.Property(e => e.CartId).HasColumnName("CART_ID");
+
+                entity.Property(e => e.ProductId)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("PRODUCT_ID");
+
+                entity.Property(e => e.CreateAt)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("CREATE_AT");
+
+                entity.Property(e => e.Price).HasColumnName("PRICE");
+
+                entity.Property(e => e.Quantity).HasColumnName("QUANTITY");
+
+                entity.Property(e => e.UserId)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("USER_ID");
+
+                entity.HasOne(d => d.Cart)
+                    .WithMany(p => p.CartDetails)
+                    .HasForeignKey(d => d.CartId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__CART_DETA__CART___69FBBC1F");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.CartDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__CART_DETA__PRODU__72910220");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.CartDetails)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__CART_DETA__USER___6BE40491");
+            });
+
             modelBuilder.Entity<Category>(entity =>
             {
                 entity.ToTable("CATEGORY");
@@ -38,6 +121,44 @@ namespace Ntier.DAL.Context
                 entity.Property(e => e.Name)
                     .HasMaxLength(20)
                     .HasColumnName("NAME");
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.ToTable("COMMENT");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.Content)
+                    .HasMaxLength(500)
+                    .HasColumnName("CONTENT");
+
+                entity.Property(e => e.CreateAt)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("CREATE_AT");
+
+                entity.Property(e => e.ProductId)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("PRODUCT_ID");
+
+                entity.Property(e => e.UserId)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("USER_ID");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__COMMENT__PRODUCT__6DCC4D03");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.Comments)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__COMMENT__USER_ID__74794A92");
             });
 
             modelBuilder.Entity<Order>(entity =>
@@ -56,10 +177,22 @@ namespace Ntier.DAL.Context
                     .IsUnicode(false)
                     .HasColumnName("CREATE_AT");
 
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("PHONE_NUMBER");
+
+                entity.Property(e => e.StatusId).HasColumnName("STATUS_ID");
+
                 entity.Property(e => e.UserId)
                     .HasMaxLength(30)
                     .IsUnicode(false)
                     .HasColumnName("USER_ID");
+
+                entity.HasOne(d => d.Status)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.StatusId)
+                    .HasConstraintName("FK__ORDERS__STATUS_I__681373AD");
 
                 entity.HasOne(d => d.User)
                     .WithMany(p => p.Orders)
@@ -117,6 +250,8 @@ namespace Ntier.DAL.Context
                 entity.Property(e => e.Name)
                     .HasMaxLength(40)
                     .HasColumnName("NAME");
+
+                entity.Property(e => e.Quantity).HasColumnName("QUANTITY");
 
                 entity.Property(e => e.Sale).HasColumnName("SALE");
 
@@ -217,6 +352,23 @@ namespace Ntier.DAL.Context
                     .HasConstraintName("FK__REFRESH_T__USERI__06CD04F7");
             });
 
+            modelBuilder.Entity<StatusOrder>(entity =>
+            {
+                entity.ToTable("STATUS_ORDER");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ID");
+
+                entity.Property(e => e.ModifiedTime)
+                    .HasColumnType("datetime")
+                    .HasColumnName("MODIFIED_TIME");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasColumnName("NAME");
+            });
+
             modelBuilder.Entity<User>(entity =>
             {
                 entity.ToTable("USER");
@@ -239,6 +391,11 @@ namespace Ntier.DAL.Context
                     .HasMaxLength(30)
                     .IsUnicode(false)
                     .HasColumnName("PASSWORD");
+
+                entity.Property(e => e.PhoneNumber)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("PHONE_NUMBER");
 
                 entity.Property(e => e.Role)
                     .HasMaxLength(10)
---
+++ Ntier.DTO/DTO/User/UserRegisterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ntier.DTO.DTO.User
+{
+    public class UserRegisterDTO
+    {
+        public string Id { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+    }
+}
